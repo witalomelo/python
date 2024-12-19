93b7751d490331b40bbfd17ef4c132d3dd30cf53
witalomelo <witalovmelo@gmail.com>
2024-12-19 10:50:29 -0300

    criando os proprios metodos

--- models/restaurante.py
+++ models/restaurante.py
@@ -1,13 +1,21 @@
 class Restaurante: 
     
+    restaurantes = []
+    
+    
     # construtor
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self) # sempre que criamos adicionamos o objeto a lista 
         
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
         
     
 restaurante_praca = Restaurante('Praca', 'Gourmet')
@@ -15,4 +23,4 @@ restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 
 
-print(restaurante_praca)
+Restaurante.listar_restaurantes()
